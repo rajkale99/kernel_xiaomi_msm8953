9b766ea346cbd01880828b5da178f52092519abf
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-10-18 13:00:30 +0700

    drivers: video: mdss, ba: power efficient workqueue intensifies
    Haha yes
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2244,7 +2244,8 @@ static int mdss_fb_blank_unblank(struct msm_fb_data_type *mfd)
 
 		/* Start the work thread to signal idle time */
 		if (mfd->idle_time)
-			schedule_delayed_work(&mfd->idle_notify_work,
+			queue_delayed_work(system_power_efficient_wq,
+				&mfd->idle_notify_work,
 				msecs_to_jiffies(mfd->idle_time));
 	}
 
@@ -5504,7 +5505,8 @@ int mdss_prim_panel_fb_unblank(int timeout)
 			atomic_set(&prim_panel_is_on, true);
 			if (timeout > 0) {
 				printk("SXF %s ,timeout  = %d\n",__func__,timeout);
-				schedule_delayed_work(&prim_panel_work, msecs_to_jiffies(timeout));
+				queue_delayed_work(system_power_efficient_wq,
+				&prim_panel_work, msecs_to_jiffies(timeout));
 			}
 			else
 				wake_unlock(&prim_panel_wakelock);
